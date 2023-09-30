69b509086583b1138ecf4dccc011d0d69c508fa9
Michal Madej <mkmadej@interia.pl>
2023-09-30 13:22:21 +0200

    viewing more info about activity, transforming into table

--- src/activity/Activity.js
+++ src/activity/Activity.js
@@ -1,14 +1,26 @@
 import '../css/activity.css';
 import ActivityDescription from "./activitydescription/ActivityDescription";
+import DeleteActivityButton from "./activitydescription/DeleteActivityButton";
+import UpdateActivityModal from "../modals/UpdateActivityModal";
 
 function Activity(props) {
-    return <div className="activity">
-                <ActivityDescription description={props.description}
-                                     time={props.time}
-                                     id={props.id}
-                                     removeActivity = {props.removeActivity}
-                                     updateActivity={props.updateActivity} />
-        </div>
+    // return <div className="activity">
+    //             <ActivityDescription description={props.description}
+    //                                  time={props.time}
+    //                                  id={props.id}
+    //                                  removeActivity = {props.removeActivity}
+    //                                  updateActivity={props.updateActivity} />
+    //     </div>
+    return (
+        <tr className="activity">
+            <td>{props.description}</td>
+            <td>{props.time}</td>
+            <td>{props.category}</td>
+            <td>{props.completed}</td>
+            <td><DeleteActivityButton id={props.id} removeActivity = {props.removeActivity} /></td>
+            <td><UpdateActivityModal id={props.id} description={props.description} time={props.time} updateActivity = {props.updateActivity} /></td>
+        </tr>
+    )
 }
 
 export default Activity;
--- src/structure/content/Activities.js
+++ src/structure/content/Activities.js
@@ -19,13 +19,30 @@ class Activities extends Component {
 
                 <h3>Activities</h3>
                 <AddActivityModal addActivity={this.props.addActivity} date={moment(new Date).format("yyyy-MM-DD")} />
-                {this.props.activities.map((activity, index) => <Activity key={index}
-                                                                          description={activity.description}
-                                                                          time={activity.timeSpentInMinutes}
-                                                                        id={activity.id}
-                                                                        removeActivity={this.props.removeActivity}
-                                                                        updateActivity={this.props.updateActivity}/>
-                )}
+                <table id="table-activities">
+                    <th>Activity</th>
+                    <th>Time spent</th>
+                    <th>Category</th>
+                    <th>Completed</th>
+                    <th>Update</th>
+                    <th>Delete</th>
+                    {this.props.activities.map((activity, index) =>  <Activity key={index}
+                                                                              description={activity.description}
+                                                                              time={activity.timeSpentInMinutes}
+                                                                               category = {activity.category}
+                                                                               completed = {activity.completed}
+                                                                              id={activity.id}
+                                                                              removeActivity={this.props.removeActivity}
+                                                                              updateActivity={this.props.updateActivity}/> )}
+                </table>
+
+                {/*{this.props.activities.map((activity, index) => <Activity key={index}*/}
+                {/*                                                          description={activity.description}*/}
+                {/*                                                          time={activity.timeSpentInMinutes}*/}
+                {/*                                                        id={activity.id}*/}
+                {/*                                                        removeActivity={this.props.removeActivity}*/}
+                {/*                                                        updateActivity={this.props.updateActivity}/>*/}
+                {/*)}*/}
             </div>
         )
     }
