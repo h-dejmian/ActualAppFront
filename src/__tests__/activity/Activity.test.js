1e6f95deb01f96325fb3d66cbab8011c37d78c18
Michal Madej <mkmadej@interia.pl>
2023-12-06 12:06:45 +0100

    activity test fix

--- src/__tests__/activity/Activity.test.js
+++ src/__tests__/activity/Activity.test.js
@@ -2,11 +2,16 @@ import {render, screen} from '@testing-library/react'
 import Activity from "../../activity/Activity";
 import {userEvent} from "@testing-library/user-event";
 
+const user = {
+    id : "519db3af-4ecf-4cce-b154-4ebad9fdea50",
+}
 
 test("Activity renders", () => {
+    //Given
     render(<Activity description="test description"
                      time="120"
-                     categoryName="test category" />);
+                     categoryName="test category"
+                      user = {user}/>);
 
     const description = screen.getByText(/test description/i)
     const time = screen.getByText(/120/i)
@@ -20,7 +25,8 @@ test("Activity renders", () => {
 test("Activity checkbox toggles after click", async () => {
     render(<Activity description="test description"
                      time="120"
-                     categoryName="test category" />);
+                     categoryName="test category"
+                      user = {user}/>);
     await userEvent.click(screen.getByRole('checkbox'));
     expect(screen.getByRole('checkbox').checked).toBe(true);
 })
